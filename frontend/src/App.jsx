81a680fa216ff340203987f1c2ebfcf8afa3db9a
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-03-20 23:29:39 +0530

    cleanup vite home page + install bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1>MERN Auth</h1>
     </>
   )
