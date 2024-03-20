d8fd77fa198d084e938a6e0c0b25cd64ae087612
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-03-20 21:00:32 +0530

    setup mongodb and connect server to mongo db

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ import dotenv from 'dotenv'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+
+connectDB();
 
 const app = express();
 
