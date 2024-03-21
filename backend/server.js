be2f580e5e56cad171dae1f56803b02257ae812d
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-03-21 12:13:38 +0530

    prepare for production

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 
@@ -15,18 +16,27 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes)
 
-app.get('/', (req, res)=> res.send('Server is ready'))
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is ready'))
+}
+
 
 
 app.use(notFound);
 app.use(errorHandler)
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server Starter on port: ${port}`)
 })
